ff170e7800fa594f4b375eaa30c10f22586d8961
Gergo N <csovis@gmail.com>
2022-12-05 16:45:27 +0100

    Add project files.

---
+++ LanguageTest/Features/Ranges.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanguageTest.Features
+{
+    internal class Ranges : IFeature
+    {
+        private readonly int[] arr;
+
+        public Ranges()
+        {
+            arr = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+        }
+
+        public void Action()
+        {
+            // indexing
+            var thirdItem = arr[3];
+            // index from end operator
+            // https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/proposals/csharp-8.0/ranges#systemindex
+            var lastItem = arr[^1];
+
+            // range operator
+            // index start, index end
+
+            // 2nd index from beginig (3rd actual item)
+            // 3rd index from last, zero based (4th actual item)
+            var sl1 = arr[2..^3];
+
+
+            var st = 1;
+            var end = 2;
+            //
+            var array = new int[] { 1, 2, 3, 4, 5 };
+            var slice1 = array[2..^3];    // array[new Range(2, new Index(3, fromEnd: true))]
+            var slice2 = array[..^3];     // array[Range.EndAt(new Index(3, fromEnd: true))]
+            var slice3 = array[2..];      // array[Range.StartAt(2)]
+            var slice4 = array[..];       // array[Range.All]
+            var slice5 = array[st..end];
+        }
+    }
+}
---
+++ LanguageTest/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LanguageTest.Features;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+Console.WriteLine("Hello, World!");
+
+
+var assembly = typeof(Program).Assembly;
+var assemblyTypes = assembly.GetTypes();
+
+foreach (Type assemblyType in assemblyTypes.Where(x => !x.IsInterface && x.GetInterfaces().Contains(typeof(IFeature))))
+{
+    Console.WriteLine(assemblyType.FullName);
+    //IFeature feature = (IFeature)Activator.CreateInstance(assemblyType);
+    //feature.Action();
+
+    Object o = assembly.CreateInstance(assemblyType.FullName, false, BindingFlags.ExactBinding,
+        null, new object[] { }, null, null);
+
+    var mInfo = assembly.GetType(assemblyType.FullName).GetMethod(nameof(IFeature.Action));
+    mInfo.Invoke(o, null);
+}
+
+if (assemblyTypes is { Length: 1 or 5 })
+{
+
+}
