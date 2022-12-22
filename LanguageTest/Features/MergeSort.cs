efd556693d2adc3b73f8b3083997add816b329e3
Gergo N <csovis@gmail.com>
2022-12-22 10:25:12 +0100

    merge sort frame

---
+++ LanguageTest/Features/MergeSort.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanguageTest.Features
+{
+    internal class MergeSort : IFeature
+    {
+        public void Action()
+        {
+            (long[] left, long[] right) divide(long[] list)
+            {
+                var halfIndex = list.Length / 2;
+                return (list[..halfIndex], list[halfIndex..]);
+            }
+            long[] sort(long[] list)
+            {
+                if (list.Length == 0) return list;
+
+
+            }
+
+            // divide
+            // conquer - sort
+            // merge
+
+        }
+    }
+}
