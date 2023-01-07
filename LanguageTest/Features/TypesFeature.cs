a7897dd0f51d028c261c820e564b57392f87067a
Gergo N <csovis@gmail.com>
2023-01-07 14:56:41 +0100

    add ValueType case

---
+++ LanguageTest/Features/TypesFeature.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanguageTest.Features
+{
+    internal class TypesFeature
+    {
+        public void test(ValueType valueType)
+        {
+            var isDec = valueType is float || valueType is double;
+        }
+    }
+}
