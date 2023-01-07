f4198e5bc6114e7483429754b19230db23bd25d5
Gergo N <csovis@gmail.com>
2023-01-07 14:56:19 +0100

    finish merge sort

--- LanguageTest/Features/MergeSort.cs
+++ LanguageTest/Features/MergeSort.cs
@@ -17,11 +17,50 @@ namespace LanguageTest.Features
             }
             long[] sort(long[] list)
             {
-                if (list.Length == 0) return list;
+                if (list.Length == 1) return list;
 
+                var merged = new long[list.Length];
+                var (left, right) = divide(list);
+                left = sort(left);
+                right = sort(right);
 
+                var righti = 0;
+                var lefti = 0;
+                var i = 0;
+                while (righti < right.Length && lefti < left.Length)
+                {
+                    if (left[lefti] < right[righti])
+                    {
+                        merged[i] = left[lefti++];
+                    }
+                    else
+                    {
+                        merged[i] = right[righti++];
+                    }
+                    i++;
+                }
+
+                while (lefti < left.Length)
+                {
+                    merged[i++] = left[lefti++];
+                }
+                while (righti < right.Length)
+                {
+                    merged[i++] = right[righti++];
+                }
+
+                return merged;
             }
 
+            var array = new long[] { 1, 51, 2, 4, 79 };
+            Console.WriteLine("Unsorted array:");
+            Console.WriteLine(string.Join(',', array));
+            var sortedArray = sort(array);
+            Console.WriteLine("Sorted array:");
+            Console.WriteLine(string.Join(',', sortedArray));
+
+            var array1 = new long[] { 79, 88, 77, 44, 55, 11 };
+            var sortedArray2 = sort(array1);
             // divide
             // conquer - sort
             // merge
