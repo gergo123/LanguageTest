f4a169d24f4871c5543d2ac837df1b394b5b5627
Gergo N <csovis@gmail.com>
2025-03-30 22:35:13 +0200

    add print matrix

---
+++ LanguageTest/Features/PrintMatrix.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanguageTest.Features
+{
+    public class PrintMatrix : IFeature
+    {
+        public void Action()
+        {
+            var matrix = new int[12][];
+            Array.Fill(matrix, new int[12]);
+
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                var row = matrix[i];
+                for (var j = 0; j < row.Length; j++)
+                {
+                    Console.Write(row[j]);
+                }
+                Console.WriteLine("---");
+            }
+        }
+    }
+}
