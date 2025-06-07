c95ba8ddb562538eefa02f919fc87ba1ae9c7a05
Gergo N <csovis@gmail.com>
2025-06-07 12:38:15 +0200

    update matrix play

--- LanguageTest/Features/PrintMatrix.cs
+++ LanguageTest/Features/PrintMatrix.cs
@@ -11,16 +11,33 @@ namespace LanguageTest.Features
         public void Action()
         {
             var matrix = new int[12][];
-            Array.Fill(matrix, new int[12]);
+            matrix = [
+                [0,0,0,0,1,1,1,0,0,0],
+                [0,0,0,1,1,1,1,0,0,0],
+                [0,0,1,1,0,1,1,0,0,0],
+                [0,1,1,0,0,1,1,0,0,0],
+                [1,1,0,0,0,1,1,0,0,0],
+                [0,0,0,0,0,1,1,0,0,0],
+                [0,0,0,0,0,1,1,0,0,0],
+                [0,0,0,0,0,1,1,0,0,0],
+                [0,0,0,0,0,1,1,0,0,0],
+            ];
+            //new int[12] { 1, },
+            //new int[12] { },
+            //new int[12] { },
+            //new int[12] { },
+            //new int[12] { },
+            //];
+            //Array.Fill(matrix, new int[12]);
 
             for (int i = 0; i < matrix.Length; i++)
             {
                 var row = matrix[i];
                 for (var j = 0; j < row.Length; j++)
                 {
-                    Console.Write(row[j]);
+                    Console.Write(row[j] == 1 ? "X" : '.');
                 }
-                Console.WriteLine("---");
+                Console.WriteLine("");
             }
         }
     }
